71d9089477259dbdf8ed0c5f8a7be1c67a44113f
Shreyas Gangurde <57534229+shreyasg-git@users.noreply.github.com>
2022-01-04 14:58:02 +0530

    validation working properly

--- src/components/InputField/InputField.jsx
+++ src/components/InputField/InputField.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 import { View, Text, TextInput, StyleSheet } from "react-native";
+import { validationStatusEnum } from "../../pages/AboutUsPage/AboutUsPage";
 
 const InputField = ({
+  formData,
+  setFormData,
   validators = () => {
     console.log("ello there");
   },
@@ -9,22 +12,35 @@ const InputField = ({
   placeholder = "placeholder",
   label = "label",
 }) => {
-  const [fieldValue, setFieldValue] = useState("");
+  const [errMsg, setErrMsg] = useState("");
   const runValidators = () => {
-    validators();
+    const { validBool, msg } = validators();
+    if (validBool) {
+      const newFormData = { ...formData };
+      newFormData[`${name}`]["validationStatus"] = validationStatusEnum.SUCCESS;
+      setFormData(newFormData);
+    } else {
+      setErrMsg(msg);
+    }
   };
   return (
     <View style={styles.view}>
       <Text style={styles.label}>{label}</Text>
       <TextInput
         style={styles.input}
-        value={fieldValue}
+        value={formData[`${name}`]["value"]}
         onChangeText={(text) => {
-          setFieldValue(text);
+          setErrMsg("");
+          const newFormData = { ...formData };
+          newFormData[`${name}`]["value"] = text;
+          setFormData(newFormData);
         }}
         placeholder={placeholder}
         onBlur={runValidators}
+        keyboardType={name === "customerPhone" ? "numeric" : null}
+        maxLength={name === "customerPhone" ? 10 : null}
       />
+      {errMsg ? <Text>{errMsg}</Text> : null}
     </View>
   );
 };
--- src/pages/AboutUsPage/AboutUsPage.jsx
+++ src/pages/AboutUsPage/AboutUsPage.jsx
@@ -1,35 +1,95 @@
-import React from "react";
-import { View, Text, Button } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, Button, TextInput } from "react-native";
 import InputField from "../../components/InputField";
 const AboutUsPage = () => {
   return (
     <View>
-      <Text>What About Us?</Text>
       <EnquiryForm />
+      {/* <Text>This</Text> */}
+      {/* <TextInput /> */}
     </View>
   );
+};
+
+export const validationStatusEnum = {
+  NOT_VALIDATED: "NOT_VALIDATED",
+  RUNNING: "RUNNING",
+  SUCCESS: "SUCCESS",
 };
 
 const EnquiryForm = () => {
+  const [formData, setFormData] = useState({
+    customerName: { value: "", validationStatus: validationStatusEnum.NOT_VALIDATED },
+    customerPhone: { value: "", validationStatus: validationStatusEnum.NOT_VALIDATED },
+    customerEmail: { value: "", validationStatus: validationStatusEnum.NOT_VALIDATED },
+    customerMsg: { value: "", validationStatus: validationStatusEnum.NOT_VALIDATED },
+  });
+  useEffect(() => {
+    console.log(formData);
+  });
+  const validateAndSubmit = () => {};
   return (
     <View>
-      <CustomForm>
-        <InputField name="customerName" label="Name: "></InputField>
-        <InputField name="customerPhone" label="Phone No.: "></InputField>
-        <InputField name="customerEmail" label="Email: "></InputField>
-        <InputField name="customerMsg" label="Message: "></InputField>
-        <Text>Enquiry Form</Text>
-        <Button title="Submit" onPress={validateAndSubmit} />
-      </CustomForm>
+      <Text>Enquiry Form</Text>
+      <InputField
+        name="customerName"
+        label="Name: "
+        formData={formData}
+        setFormData={setFormData}
+        validators={() => {
+          if (formData.customerName.value.length === 0) {
+            return { validBool: false, msg: "plz enter Name" };
+          }
+          return { validBool: true, msg: "success" };
+        }}
+      />
+      <InputField
+        name="customerPhone"
+        label="Phone : (10 digits only)"
+        formData={formData}
+        setFormData={setFormData}
+        validators={() => {
+          const regExNumbersOnly = /^\d+$/;
+          if (formData.customerPhone.value.length === 10) {
+            if (formData.customerPhone.value.toString().match(regExNumbersOnly)) {
+              return { validBool: true, msg: "success" };
+            } else {
+              return { validBool: false, msg: "Phone Number Not Valid" };
+            }
+          } else {
+            return { validBool: false, msg: "Phone Number Not Valid 2" };
+          }
+        }}
+      />
+      <InputField
+        name="customerEmail"
+        label="Email: "
+        formData={formData}
+        setFormData={setFormData}
+        validators={() => {
+          let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+          if (formData.customerEmail.value.match(regexEmail)) {
+            return { validBool: true, msg: "success" };
+          }
+          return { validBool: false, msg: "Please Enter Valid Email" };
+        }}
+      />
+      <InputField
+        name="customerMsg"
+        label="Message: "
+        formData={formData}
+        setFormData={setFormData}
+        validators={() => {
+          // const regExNumbersOnly = /^d{10}$/;
+          // if (formData.customerPhone.value.match(regExNumbersOnly)) {
+          //   return { validBool: false, msg: "Phone Number Not Valid" };
+          // }
+          return { validBool: true, msg: "success" };
+        }}
+      />
+      <Button name="submit" title="Submit" onPress={validateAndSubmit} setFormData={setFormData} />
     </View>
   );
-};
-
-const CustomForm = ({ children }) => {
-  validateAndSubmit = () => {
-    console.log("Validation Run Success");
-  };
-  return <View>{children}</View>;
 };
 
 export default AboutUsPage;
